ecd2f55d9713664830e56747e5f8798165594e2b
amir-r-z-a <ahmadiamirreza81@gmail.com>
2023-07-25 13:20:40 +0330

    add time functionality

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,9 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from datetime import datetime, timedelta
 
 
 class Question(models.Model):
     def __str__(self):
         return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= (timezone.now() - timedelta(days=1)).timestamp()
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
 
@@ -11,6 +17,7 @@ class Question(models.Model):
 class Choice(models.Model):
     def __str__(self):
         return self.choice_text
+
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
